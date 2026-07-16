210e433331aa3d8621dda8d03ed009ad46519d62
Azman988 <azeezusman571@gmail.com>
2026-07-16 21:13:41 +0100

    feat: add server proxy configuration for API requests

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || '/ar-rahmoh/'
+    base: process.env.VITE_BASE_PATH || '/ar-rahmoh/',
+    server: {
+        proxy: {
+        // Intercepts any frontend requests starting with /api
+        '/api': {
+            target: 'http://localhost:5000', // Your Express backend URL
+            changeOrigin: true,
+            secure: false,
+        }
+        }
+    }
 }) 
