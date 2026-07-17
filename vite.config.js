55fbb1ce761ab65008c23db6cd87a51a49931e5d
Azman988 <azeezusman571@gmail.com>
2026-07-17 11:40:31 +0100

    fix: update base path in vite.config.js for consistency and clarity

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || '/ar-rahmoh/',
+    base: '/',
     server: {
         proxy: {
-        // Intercepts any frontend requests starting with /api
-        base: './',
         '/api': {
-            target: 'http://localhost:5000', // Your Express backend URL
+            target: 'http://localhost:5000', //Express backend URL
             changeOrigin: true,
             secure: false,
         }
